53275203594aa4d60b8f0a09c46bade6fcdfd9d2
Ru Yi Shen <ruyishen2012@gmail.com>
2022-07-05 18:21:27 -0300

    feat : auth router - status ok

--- src/app.js
+++ src/app.js
@@ -3,10 +3,12 @@ import cors from "cors";
 import express from "express";
 import jwt from "jsonwebtoken";
 import connection from "./database.js";
+import router from "./routers/index.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(router);
 
 app.post("/sign-up", async (req, res) => {
   try {
@@ -30,84 +32,6 @@ app.post("/sign-up", async (req, res) => {
     await connection.query(
       `INSERT INTO "users" ("name", "email", "password") VALUES ($1, $2, $3)`,
       [name, email, hashedPassword]
-    );
-
-    res.sendStatus(201);
-  } catch (err) {
-    console.error(err);
-    res.sendStatus(500);
-  }
-});
-
-app.post("/sign-in", async (req, res) => {
-  try {
-    const { email, password } = req.body;
-
-    if (!email || !password) {
-      return res.sendStatus(422);
-    }
-
-    const { rows } = await connection.query(
-      `SELECT * FROM "users" WHERE "email"=$1`,
-      [email]
-    );
-    const [user] = rows;
-
-    if (!user || !bcrypt.compareSync(password, user.password)) {
-      return res.sendStatus(401);
-    }
-
-    const token = jwt.sign(
-      {
-        id: user.id,
-      },
-      process.env.JWT_SECRET
-    );
-
-    res.send({
-      token,
-    });
-  } catch (err) {
-    console.error(err);
-    res.sendStatus(500);
-  }
-});
-
-app.post("/financial-events", async (req, res) => {
-  try {
-    const authorization = req.headers.authorization || "";
-    const token = authorization.replace("Bearer ", "");
-
-    if (!token) {
-      return res.sendStatus(401);
-    }
-
-    let user;
-
-    try {
-      user = jwt.verify(token, process.env.JWT_SECRET);
-    } catch {
-      return res.sendStatus(401);
-    }
-
-    const { value, type } = req.body;
-
-    if (!value || !type) {
-      return res.sendStatus(422);
-    }
-
-    const financialTypes = ["INCOME", "OUTCOME"];
-    if (!financialTypes.includes(type)) {
-      return res.sendStatus(422);
-    }
-
-    if (value < 0) {
-      return res.sendStatus(422);
-    }
-
-    await connection.query(
-      `INSERT INTO "financialEvents" ("userId", "value", "type") VALUES ($1, $2, $3)`,
-      [user.id, value, type]
     );
 
     res.sendStatus(201);
